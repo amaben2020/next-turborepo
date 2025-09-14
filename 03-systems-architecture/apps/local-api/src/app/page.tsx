bf25af24b14d10fe4cfc6507b78faa375b4b7197
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-09-14 11:39:48 +0100

    adding legacy app to test deployment

--- 03-systems-architecture/apps/local-api/src/app/page.tsx
+++ 03-systems-architecture/apps/local-api/src/app/page.tsx
@@ -1,12 +1,14 @@
-import { auth } from "@/auth";
-import { Todo, PRIORITIES, getTodos } from "@repo/todos";
+import { auth } from '@/auth';
+import { Todo, PRIORITIES, getTodos } from '@repo/todos';
 
-import AuthButton from "@/components/AuthButton.server";
+import AuthButton from '@/components/AuthButton.server';
 
-import TodoList from "./TodoList";
+import TodoList from './TodoList';
 
 export default async function Home() {
   const session = await auth();
+
+  console.log(session);
 
   const priorities: string[] = PRIORITIES;
 
@@ -16,8 +18,15 @@ export default async function Home() {
 
   return (
     <main>
-      <AuthButton />
-      {session?.user && <TodoList todos={todos} priorities={priorities} />}
+      {/* <AuthButton />
+      {session?.user && <TodoList todos={todos} priorities={priorities} />} */}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        quam, reprehenderit, animi porro totam, atque sunt laborum recusandae
+        neque sit fuga pariatur illum vero aut. Quod quidem assumenda fugiat
+        accusamus?
+      </p>
+      <TodoList todos={todos} priorities={priorities} />
     </main>
   );
 }
--- 03-systems-architecture/apps/local-api/src/middleware.ts
+++ 03-systems-architecture/apps/local-api/src/middleware.ts
@@ -1,13 +1,14 @@
-import { NextResponse } from "next/server";
-import { auth } from "@/auth";
+import { NextResponse } from 'next/server';
+import { auth } from '@/auth';
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|/).*)"],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|/).*)'],
 };
 
 export default auth((req) => {
   const reqUrl = new URL(req.url);
-  if (!req.auth && reqUrl?.pathname !== "/") {
-    return NextResponse.redirect(new URL("/api/auth/signin", req.url));
-  }
+  // if (!req.auth && reqUrl?.pathname !== "/") {
+  //   return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+  // }
+  return NextResponse.next();
 });
